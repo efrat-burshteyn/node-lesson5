0b1eae3d66ffa440fcf1110153283e2f747a9095
efrat-burshteyn <efrat4687@gmail.com>
2026-07-14 22:44:53 +0300

    books

---
+++ db.js
@@ -0,0 +1,28 @@
+export const books=[
+  {
+        id: 1,
+        name: "מדריך לתכנות בסיסי",
+        category: "מחשבים",
+        price: 95,
+        isAvailable: true,
+        loans: []
+    },
+    {
+        id: 2,
+        name: "היסטוריה של העולם",
+        category: "היסטוריה",
+        price: 120,
+        isAvailable: false,
+        loans: [
+            { date: "2026-07-10", customerId: 101 }
+        ]
+    },
+    {
+        id: 3,
+        name: "מתמטיקה שימושית",
+        category: "מתמטיקה",
+        price: 75,
+        isAvailable: true,
+        loans: []
+    }
+]
