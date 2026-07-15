de599fb24b17fee3eefbed6e94884330f74f0a34
efrat-burshteyn <efrat4687@gmail.com>
2026-07-15 22:01:46 +0300

    arr users

---
+++ users.js
@@ -0,0 +1,23 @@
+export const users=[
+    {
+        id: 1,
+        username: "yossi_cohen",
+        email: "yossi@example.com",
+        password: "password123",
+        borrowedBooks: [1]
+    },
+    {
+        id: 2,
+        username: "dina_levy",
+        email: "dina@example.com",
+        password: "securePass456",
+        borrowedBooks: []
+    },
+    {
+        id: 3,
+        username: "avi_israeli",
+        email: "avi@example.com",
+        password: "mySecret789",
+        borrowedBooks: [2,3]
+    }
+];
