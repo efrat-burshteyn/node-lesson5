d515e46b13b4bdea8a2462556d2d83abd86d2873
efrat-burshteyn <efrat4687@gmail.com>
2026-07-16 22:27:24 +0300

    add cors to code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import cors from 'cors';
 import express from 'express';
 import mainRouter from "./routes/index.route.js";
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api',mainRouter);
 
@@ -8,3 +10,17 @@ app.use('/api',mainRouter);
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000')
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
